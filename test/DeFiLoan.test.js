f346e9db3f76244d050057f80ba0361d4cb12d7a
Sumururduc Alexandru <sumurduc2003@gmail.com>
2025-05-16 01:34:42 +0300

    Update tests

--- test/DeFiLoan.test.js
+++ test/DeFiLoan.test.js
@@ -2,9 +2,9 @@ const truffleAssert = require('truffle-assertions');
 const LendingAndLoaning = artifacts.require("LendingAndLoaning");
 
 contract("LendingAndLoaning", (accounts) => {
-    const lender = accounts[0]; // Adresa lender-ului
-    const borrower = accounts[1]; // Adresa borrower-ului
-    const lendAmount = web3.utils.toWei("1", "ether"); // Suma de Ã®mprumut (1 ETH)
+    const lender = accounts[0]; 
+    const borrower = accounts[1]; 
+    const lendAmount = web3.utils.toWei("1", "ether"); 
 
     let lendingContract;
 
@@ -87,6 +87,107 @@ contract("LendingAndLoaning", (accounts) => {
         lendingContract.payLoan(borrower, borrowerLoans.length -1, { from: borrower, value: repayAmount }),
         "Loan already repaid",
         "Should not allow double repayment"
-    );
+        );
     });
+    
+    //integritate test
+    it("should allow full lending and borrowing cycle", async () => {
+        const loanAmount = web3.utils.toWei("1", "ether");
+        const dueDate = Math.floor(Date.now() / 1000) + 3600;
+    
+        
+        await lendingContract.lend(borrower, loanAmount, { from: lender, value: loanAmount });
+    
+       
+        await lendingContract.createLoan(loanAmount, dueDate, { from: borrower });
+    
+       
+        await lendingContract.payLoan(borrower, 0, { from: lender, value: loanAmount });
+    
+        const paidLoans = await lendingContract.getPaidLoans(borrower);
+        assert.equal(paidLoans.length, 1, "Loan should be marked as paid");
+    });
+
+
+    //performanta test
+    it("should measure gas used for lending operation", async () => {
+        const tx = await lendingContract.lend(borrower, lendAmount, { from: lender, value: lendAmount });
+        console.log("Gas used for lending:", tx.receipt.gasUsed);
+    
+        assert.isBelow(tx.receipt.gasUsed, 200000, "Gas usage is too high");
+    });
+    it("should measure gas used for creating a loan", async () => {
+        const dueDate = Math.floor(Date.now() / 1000) + 3600;
+        
+      
+        await lendingContract.lend(borrower, lendAmount, {
+            from: lender,
+            value: lendAmount
+        });
+    
+        const tx = await lendingContract.createLoan(lendAmount, dueDate, {
+            from: borrower
+        });
+    
+        console.log("Gas used for createLoan:", tx.receipt.gasUsed);
+        assert.isBelow(tx.receipt.gasUsed, 200000, "Gas usage for createLoan is too high");
+    });
+    it("should measure gas used for paying back a loan", async () => {
+        const dueDate = Math.floor(Date.now() / 1000) + 3600;
+    
+        
+        await lendingContract.lend(borrower, lendAmount, {
+            from: lender,
+            value: lendAmount
+        });
+    
+        await lendingContract.createLoan(lendAmount, dueDate, {
+            from: borrower
+        });
+    
+        const tx = await lendingContract.payLoan(borrower, 0, {
+            from: lender,
+            value: lendAmount
+        });
+    
+        console.log("Gas used for payLoan:", tx.receipt.gasUsed);
+        assert.isBelow(tx.receipt.gasUsed, 200000, "Gas usage for payLoan is too high");
+    });
+    it("should measure gas used for lending and taking back the lending", async () => {
+        await lendingContract.lend(borrower, lendAmount, { from: lender, value: lendAmount });
+
+        const tx = await lendingContract.withdraw();
+
+        console.log("Gas used for withdraw:", tx.receipt.gasUsed);
+    
+        assert.isBelow(tx.receipt.gasUsed, 200000, "Gas usage is too high");
+ 
+    });
+
 });
+
+
+///Chat GPT: 
+/*
+contract("LendingAndLoaning", (accounts) => {
+  const lender = accounts[0];
+  const borrower = accounts[1];
+  const lendAmount = web3.utils.toWei("1", "ether");
+
+  let instance;
+
+  beforeEach(async () => {
+    instance = await LendingAndLoaning.new();
+  });
+
+  it("should correctly record ETH lent and update lenders array", async () => {
+    await instance.lend(borrower, lendAmount, { from: lender, value: lendAmount });
+
+    const recordedAmount = await instance.getEthLentAmount(borrower);
+    assert.equal(recordedAmount.toString(), lendAmount, "Lent amount is incorrect");
+
+    const lenders = await instance.getLenders();
+    assert.include(lenders, borrower, "Borrower not added to lenders list");
+  });
+});
+*/
